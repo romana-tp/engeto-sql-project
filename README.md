e9fdcfb7baacdc2bc0acfed1cbf814f12a47d6ff
Romana Tomeckova Pistekova <romanapistekova@seznam.com>
2025-08-28 21:24:42 +0200

    Answer question 2

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # SQL Project for Engeto Data Academy
 
-Here is an intro to my SQL project for Engeto Data Academy. 
+Intro: A project focused on assessing the availability of basic foodstuffs to the general public.
 
-Poznámka o chybě v tisících zaměstnanců. V korunách v calculation Code nebo unit Code
+Note: There was a mistake in the original dataset regarding "Calculation code" and "Unit code", which had to be corrected. Values - "thousands of employees" and "CZK" was mixed.
 
 
 1. Resarch question no.1: Do salaries increase across all industry branches, or are there any decreases as well?
@@ -10,3 +10,8 @@ Poznámka o chybě v tisících zaměstnanců. V korunách v calculation Code ne
 Since the full data on employee counts for each year is not available, I decided to calculate unweighted averages. First, I created a temporary table (by using CTE) and joined it with another one containing the names of the industry branch codes. Using the LAG function, I created a new column showing the differences between years. By ordering the results by these values, I was able to answer the first question.
 
 Ansnwer: There are a few industry branches and payroll years where we can see a decrease. Specifically, we talk about 32 cases. The biggest decrease happened in 2013 in Financial services and Insurance Industry (code K) - there was a 4 484 CZK decrease comparing to the previous year.
+
+2. Research question no. 2: How many liters of milk and kilograms of bread can be purchased in the first and last comparable periods in the available price and wage data?
+
+Answer: According to the resulting table, in the first comparable period (the first quarter of 2006), it was possible to buy 1,252 units (kg) of bread, and in the last period (the fourth quarter of 2018), 1,426 units (kg) of bread.
+In the same periods, it was possible to purchase 1,284 units (liters) of milk in the first period, and 1,749 units (liters) of milk in the second period.
--- projekt1.sql
+++ projekt1.sql
@@ -4,16 +4,16 @@
 WITH prumernamzda as (
 SELECT 
 ROUND (AVG(cp.value)::numeric,0) as AvgPerYear, industry_branch_code , payroll_year
-from czechia_payroll as cp
-WHERE unit_code = '200' and calculation_code = '200' and value IS NOT NULL and value_type_code  = '5958' and industry_branch_code IS NOT NULL
-group by payroll_year, industry_branch_code
-order by 
+FROM czechia_payroll as cp
+WHERE unit_code = '200' AND calculation_code = '200' AND value IS NOT NULL and value_type_code  = '5958' and industry_branch_code IS NOT NULL
+GROUP BY payroll_year, industry_branch_code
+ORDER BY 
 cp.payroll_year, industry_branch_code
 )
 SELECT
 payroll_year, name,code, 
 avgperyear - (LAG (avgperyear) OVER (PARTITION BY industry_branch_code ORDER BY payroll_year))
- as Differences
+ AS Differences
 FROM
 prumernamzda as pm JOIN  czechia_payroll_industry_branch as cpib on pm.industry_branch_code = cpib.code
 ORDER BY 
@@ -22,55 +22,7 @@ Differences;
  /* Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední 
  * srovnatelné období v dostupných datech cen a mezd?*/
 
-WITH cistka as 
-	(SELECT 
-	value, 
-	category_code, 
-	date_part('year', date_from) as Year
-	FROM
-	czechia_price as cp 
-		where (category_code = '114201' and region_code IS NULL)
-		OR (category_code = '111301' and region_code IS NULL)
-	group by year, value, category_code
-	order by year, category_code
-	), 
-VypocetPrumerneMzdy as (
-	SELECT 
-	payroll_year,
-	payroll_quarter,
-	AVG (value) as PrumernaMzda 
-	from czechia_payroll 
-	where value_type_code = '5958' 
-	and unit_code = '200' 
-	and calculation_code = '200' 
-	and industry_branch_code is null and value is not null
-	GROUP BY 
-	payroll_year, payroll_quarter
-	ORDER BY payroll_year)
-SELECT
-c.category_code, cpc.name, cpc.price_unit, c.comparing
-from cistka as c
-join czechia_price_category as cpc on c.category_code=cpc.code 
-where comparing IS NOT NULL
-
-
-SELECT 
-MIN (date_from) as PrvniObdobi, 
-MAX (date_to) as PosledniObdobi
-FROM
-czechia_price as cp 
-
-SELECT payroll_year, AVG (value) as PrumernaMzda 
-from czechia_payroll 
-where value_type_code = '5958' 
-and unit_code = '200' 
-and calculation_code = '200' 
-and industry_branch_code is null and value is not null
-GROUP BY 
-payroll_year
-ORDER BY payroll_year 
-
-with test as (
+WITH CenyPrumer as (
 	SELECT 
 	value, 
 	category_code, 
@@ -85,12 +37,28 @@ with test as (
 		where  region_code IS NOT NULL
 	group by  value, category_code, price_quarter , year
 	order by  category_code
+	),
+VypocetPrumerneMzdy as (
+	SELECT 
+	payroll_year,
+	payroll_quarter,
+	AVG (value) as PrumernaMzda 
+	from czechia_payroll 
+	where value_type_code = '5958' 
+	and unit_code = '200' 
+	and calculation_code = '200' 
+	and industry_branch_code is null and value is not null
+	GROUP BY 
+	payroll_year, payroll_quarter
+	ORDER BY payroll_year
 	)
 SELECT
-	AVG(value), test.year, test.price_quarter, test.category_code, cpc.name
-	from test
-	join czechia_price_category as cpc on test.category_code=cpc.code
-	group by year, price_quarter, category_code, cpc.name
-	order by cpc.name, year, price_quarter , category_code 
+	AVG(value) as PrumCenaZaJednotku, cpr.year, cpr.price_quarter, cpr.category_code, cpc.name, prumernamzda, ROUND ((prumernamzda/AVG(value))::numeric, 0) as PocetZakoupenychJednotek
+	from CenyPrumer cpr
+	join czechia_price_category as cpc on cpr.category_code=cpc.code
+	join VypocetPrumerneMzdy as vpm on vpm.payroll_year=cpr.year and vpm.payroll_quarter=cpr.price_quarter 
+	where cpr.category_code in ('111301','114201') and ((cpr.year = '2006' and cpr.price_quarter = '1') or (cpr.year = '2018' and cpr.price_quarter = '4'))
+	group by year, price_quarter, category_code, cpc.name, prumernamzda 
+	order by cpc.name, year, price_quarter , category_code;
 
 
